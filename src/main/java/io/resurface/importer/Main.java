3eb213acddc56ed3bd0224e2636fa24207e66601
Rob Dickinson <robfromboulder@gmail.com>
2021-07-07 17:38:36 -0600

    Post NDJSON for better performance

--- src/main/java/io/resurface/importer/Main.java
+++ src/main/java/io/resurface/importer/Main.java
@@ -9,6 +9,11 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.zip.DeflaterOutputStream;
 
 /**
  * Imports data to Resurface database.
@@ -16,12 +21,15 @@ import java.nio.charset.StandardCharsets;
 public class Main {
 
     /**
+     * Number of messages written at once.
+     */
+    public static final int BATCH_SIZE = 32;
+
+    /**
      * Runs importer as command-line program.
      */
     public static void main(String[] args) throws Exception {
-        System.out.println("\n>>> Importer starting");
         new Main();
-        System.out.println(">>> Importer finished!\n");
     }
 
     /**
@@ -41,38 +49,75 @@ public class Main {
         System.out.println("URL=" + url);
         parsed_url = new URL(url);
 
-        // send all lines
+        // send all lines in batches
+        new Thread(new BatchSender()).start();
         try (MessageFileReader reader = new MessageFileReader(file)) {
-            reader.iterate(this::send);
+            reader.iterate((line) -> {
+                try {
+                    this.batch.add(line);
+                    if (this.batch.size() == BATCH_SIZE) {
+                        ArrayList<String> current_batch = this.batch;
+                        this.batch = new ArrayList<>();
+                        batch_queue.put(current_batch);
+                    }
+                } catch (RuntimeException | InterruptedException re) {
+                    // do nothing
+                }
+            });
         }
 
-        status();  // show final status
+        // flush last messages
+        batch_queue.put(this.batch);
     }
 
     /**
-     * Send raw message to the target URL.
+     * Worker thread that sends batches of messages.
      */
-    private void send(String message) {
-        try {
-            HttpURLConnection url_connection = (HttpURLConnection) parsed_url.openConnection();
-            url_connection.setConnectTimeout(5000);
-            url_connection.setReadTimeout(5000);
-            url_connection.setRequestMethod("POST");
-            url_connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            url_connection.setDoOutput(true);
-            try (OutputStream os = url_connection.getOutputStream()) {
-                os.write(message.getBytes(StandardCharsets.UTF_8));
-                os.flush();
+    class BatchSender implements Runnable {
+
+        public void run() {
+            boolean polling = true;
+            try {
+                while (polling) {
+                    List<String> b = batch_queue.take();
+
+                    // make request to database
+                    HttpURLConnection url_connection = (HttpURLConnection) parsed_url.openConnection();
+                    url_connection.setConnectTimeout(5000);
+                    url_connection.setReadTimeout(5000);
+                    url_connection.setRequestMethod("POST");
+                    url_connection.setRequestProperty("Content-Encoding", "deflated");
+                    url_connection.setRequestProperty("Content-Type", "application/ndjson; charset=UTF-8");
+                    url_connection.setRequestProperty("User-Agent", "Resurface/2.x (importer)");
+                    url_connection.setDoOutput(true);
+                    try (OutputStream os = url_connection.getOutputStream()) {
+                        try (DeflaterOutputStream dos = new DeflaterOutputStream(os, true)) {
+                            for (String m : b) {
+                                dos.write(m.getBytes(StandardCharsets.UTF_8));
+                                dos.write("\n".getBytes(StandardCharsets.UTF_8));
+                            }
+                            dos.finish();
+                            dos.flush();
+                        }
+                        os.flush();
+                    }
+
+                    // check response from database
+                    int response_code = url_connection.getResponseCode();
+                    if (response_code != 204) {
+                        System.out.println("Failed with response code: " + response_code);
+                    }
+
+                    // update running state
+                    polling = (b.size() == BATCH_SIZE);
+                    messages_written += b.size();
+                    status();
+                }
+            } catch (RuntimeException | IOException | InterruptedException e) {
+                e.printStackTrace();
             }
-            int response_code = url_connection.getResponseCode();
-            if (response_code == 204) {
-                if (messages_written++ % 100 == 0) status();
-            } else {
-                System.out.println("Failed with response code: " + response_code);
-            }
-        } catch (IOException ioe) {
-            throw new RuntimeException(ioe);
         }
+
     }
 
     /**
@@ -81,11 +126,13 @@ public class Main {
     private void status() {
         long elapsed = System.currentTimeMillis() - started;
         long rate = (messages_written * 1000 / elapsed);
-        System.out.println("Messages: " + messages_written + ", Elapsed time: " + elapsed
-                + " ms, Rate: " + rate + " msg/sec");
+        System.out.println("Messages: " + messages_written + ", Elapsed time: " + elapsed + " ms, Rate: " + rate + " msg/sec");
     }
 
+    private ArrayList<String> batch = new ArrayList<>();
+    private final BlockingQueue<List<String>> batch_queue = new ArrayBlockingQueue<>(128);
     private long messages_written = 0;
     private final URL parsed_url;
     private final long started = System.currentTimeMillis();
+
 }
