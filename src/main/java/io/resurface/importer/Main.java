57812a8de4b48adf7753f74ac9af6256e86585db
Rob Dickinson <robfromboulder@gmail.com>
2022-10-04 01:19:11 -0600

    Simplify thread polling, remove race conditions, fix url calculation based on ports

--- src/main/java/io/resurface/importer/Main.java
+++ src/main/java/io/resurface/importer/Main.java
@@ -4,11 +4,7 @@ package io.resurface.importer;
 
 import io.resurface.messages.MessageFileReader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -36,13 +32,15 @@ public class Main {
     }
 
     /**
-     * Reads the target file and sends each line as a logger message.
+     * Reads the input file and sends each line as a logger message.
      */
     public Main() throws Exception {
-        // read configuration
+        // read location of input file
         String file = System.getProperty("FILE");
         if (file == null) throw new IllegalArgumentException("Missing FILE");
         System.out.println("FILE=" + file);
+
+        // calculate destination url if not provided
         String url = System.getProperty("URL");
         if (url == null) {
             String host = System.getProperty("HOST");
@@ -51,56 +49,57 @@ public class Main {
             String port = System.getProperty("PORT");
             if (port == null) port = "7701";
             System.out.println("PORT=" + port);
-            // calculate url
-            url = "http://" + host + ":" + port + "/message";
+            if (port.equals("80") || port.equals("443")) {
+                url = (port.equals("443") ? "https://" : "http://") + host + "/fluke/message";
+            } else {
+                url = "http://" + host + ":" + port + "/message";
+            }
         }
         System.out.println("URL=" + url);
         parsed_url = new URL(url);
-        
+
+        // read repeat & saturation_stop options
         String repeat = System.getProperty("REPEAT");
         if (repeat == null) repeat = "yes";
         System.out.println("REPEAT=" + repeat);
-
         if ("yes".equalsIgnoreCase(repeat)) {
-            String stop_when_saturated = System.getProperty("SATURATED_STOP");
-            if (stop_when_saturated == null) stop_when_saturated = "no";
-            System.out.println("SATURATED_STOP=" + stop_when_saturated);
-            if ("yes".equalsIgnoreCase(stop_when_saturated)) {
-                parsed_saturated_url = new URL(url.substring(0, url.indexOf("/", 7)) + "/saturated");
-            } else {
-                parsed_saturated_url = null;
+            String saturated_stop = System.getProperty("SATURATED_STOP");
+            if (saturated_stop == null) saturated_stop = "no";
+            System.out.println("SATURATED_STOP=" + saturated_stop);
+            if ("yes".equalsIgnoreCase(saturated_stop)) {
+                saturated_url = new URL(url.substring(0, url.lastIndexOf("/")) + "/saturated");
+                System.out.println("SATURATED_URL=" + saturated_url);
             }
-        } else {
-            parsed_saturated_url = null;
         }
 
-        // send all lines in batches
+        // create thread to send batches asynchronously
         new Thread(new BatchSender()).start();
 
-        // send messages in a loop until terminated
-        boolean keep_going = true;
-        while (keep_going) {
+        // read file lines and submit messages in batches
+        do {
             try (MessageFileReader reader = new MessageFileReader(file)) {
                 reader.iterate((line) -> {
                     try {
                         this.batch.add(line);
-                        if (this.batch.size() == BATCH_SIZE) {
-                            ArrayList<String> current_batch = this.batch;
-                            this.batch = new ArrayList<>();
-                            batch_queue.put(current_batch);
-                        }
+                        if (this.batch.size() == BATCH_SIZE) submit_current_batch();
                     } catch (RuntimeException | InterruptedException re) {
                         // do nothing
                     }
                 });
             }
+            submit_current_batch();  // flush lingering messages
+        } while ("yes".equalsIgnoreCase(repeat));
 
-            // flush last messages
-            batch_queue.put(this.batch);
+        // signal sender thread to stop via poison
+        batch_queue.put(POISON_BATCH);
+    }
 
-            // exit if necessary
-            keep_going = "yes".equalsIgnoreCase(repeat) && !saturated;
-        }
+    /**
+     * Submits the current batch to be sent and creates a new empty batch.
+     */
+    private void submit_current_batch() throws InterruptedException {
+        batch_queue.put(this.batch);
+        this.batch = new ArrayList<>();
     }
 
     /**
@@ -109,21 +108,38 @@ public class Main {
     class BatchSender implements Runnable {
 
         public void run() {
-            boolean polling = true;
             try {
-                while (polling) {
+                while (true) {
                     List<String> b = batch_queue.take();
 
+                    // exit if poisoned
+                    if (b == POISON_BATCH) System.exit(0);
+
+                    // exit if database is saturated
+                    if (saturated_url != null) {
+                        HttpURLConnection c = (HttpURLConnection) saturated_url.openConnection();
+                        c.setConnectTimeout(5000);
+                        c.setReadTimeout(5000);
+                        c.setRequestMethod("GET");
+                        boolean saturated;
+                        try (InputStream is = c.getInputStream()) {
+                            try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+                                saturated = "true".equals(br.readLine());
+                            }
+                        }
+                        if (saturated) System.exit(0);
+                    }
+
                     // make request to database
-                    HttpURLConnection url_connection = (HttpURLConnection) parsed_url.openConnection();
-                    url_connection.setConnectTimeout(5000);
-                    url_connection.setReadTimeout(5000);
-                    url_connection.setRequestMethod("POST");
-                    url_connection.setRequestProperty("Content-Encoding", "deflated");
-                    url_connection.setRequestProperty("Content-Type", "application/ndjson; charset=UTF-8");
-                    url_connection.setRequestProperty("User-Agent", "Resurface/2.x (importer)");
-                    url_connection.setDoOutput(true);
-                    try (OutputStream os = url_connection.getOutputStream()) {
+                    HttpURLConnection c = (HttpURLConnection) parsed_url.openConnection();
+                    c.setConnectTimeout(5000);
+                    c.setReadTimeout(5000);
+                    c.setRequestMethod("POST");
+                    c.setRequestProperty("Content-Encoding", "deflated");
+                    c.setRequestProperty("Content-Type", "application/ndjson; charset=UTF-8");
+                    c.setRequestProperty("User-Agent", "Resurface/v3.3.x (importer)");
+                    c.setDoOutput(true);
+                    try (OutputStream os = c.getOutputStream()) {
                         try (DeflaterOutputStream dos = new DeflaterOutputStream(os, true)) {
                             for (String m : b) {
                                 dos.write(m.getBytes(StandardCharsets.UTF_8));
@@ -136,27 +152,13 @@ public class Main {
                     }
 
                     // check response from database
-                    int response_code = url_connection.getResponseCode();
+                    int response_code = c.getResponseCode();
                     if (response_code != 204) {
                         System.out.println("Failed with response code: " + response_code);
-                    }
-
-                    // check if database is saturated
-                    if (!saturated && parsed_saturated_url != null) {
-                        HttpURLConnection sat_url_connection = (HttpURLConnection) parsed_saturated_url.openConnection();
-                        sat_url_connection.setConnectTimeout(5000);
-                        sat_url_connection.setReadTimeout(5000);
-                        sat_url_connection.setRequestMethod("GET");
-                        try (InputStream is = sat_url_connection.getInputStream()) {
-                            try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-                                saturated = "true".equals(br.readLine());
-                            }
-                        }
-
+                        System.exit(-1);
                     }
 
                     // update running state
-                    polling = (b.size() == BATCH_SIZE) && !saturated;
                     messages_written += b.size();
                     status();
                 }
@@ -173,16 +175,16 @@ public class Main {
     private void status() {
         long elapsed = System.currentTimeMillis() - started;
         long rate = (messages_written * 1000 / elapsed);
-        String saturated_string = (parsed_saturated_url != null) && saturated ? " - SATURATED" : "";
-        System.out.println("Messages: " + messages_written + ", Elapsed time: " + elapsed + " ms, Rate: " + rate + " msg/sec" + saturated_string);
+        System.out.println("Messages: " + messages_written + ", Elapsed time: " + elapsed + " ms, Rate: " + rate + " msg/sec");
     }
 
     private ArrayList<String> batch = new ArrayList<>();
     private final BlockingQueue<List<String>> batch_queue = new ArrayBlockingQueue<>(128);
     private long messages_written = 0;
-    private boolean saturated = false;
     private final URL parsed_url;
-    private final URL parsed_saturated_url;
+    private URL saturated_url;
     private final long started = System.currentTimeMillis();
+
+    private static final List<String> POISON_BATCH = new ArrayList<>();
 
 }
